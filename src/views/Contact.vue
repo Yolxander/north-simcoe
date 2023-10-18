1a03129b3fae5b7cbe6dea47c0bbb94f3ccd14d8
YolxanderHC <yolxander.jaca@humancontact.com>
2023-10-17 22:36:33 -0400

    added map to contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,42 @@
 <template>
-  <HeroGlobal :hero="hero" />
-  <ContactUs />
+    <HeroGlobal :hero="hero"/>
+    <div className="max-w-screen-xl p-10 md:p-32 mx-auto flex flex-col md:flex-row md:items-center md:space-x-8 md:gap-5">
+        <!-- Left Section -->
+        <div className="py-10 md:py-0 md:w-3/5">
+            <h1 className="text-3xl font-extrabold mb-4 text-brown font-libre-baskerville">Innovative</h1>
+            <hr className="w-10 h-1 my-4 border-0 rounded bg-teal"/>
+            <p className="text-gray-600 mb-6">Get in touch with us for any inquiries or collaborations.</p>
+
+            <!-- Email Section -->
+            <div className="flex items-center mb-4">
+                <font-awesome-icon :icon="['fas', 'envelope']" class="text-teal mr-4"/>
+                <a href="mailto:hello@myemaildigital.com"
+                   className="text-2xl font-extrabold mb-4 text-brown font-libre-baskerville hover:underline">hello@myemaildigital.com</a>
+            </div>
+
+            <!-- Phone Section -->
+            <div className="flex items-center mb-4">
+                <font-awesome-icon :icon="['fas', 'phone']" class="text-teal mr-4"/>
+                <a href="tel:+1(555)-123-4567"
+                   className="text-2xl font-extrabold mb-4 text-brown font-libre-baskerville hover:underline">+1(555)-123-4567</a>
+            </div>
+
+            <!-- Office Section -->
+            <div className="flex items-center mb-4">
+                <font-awesome-icon :icon="['fas', 'map-marker-alt']" class="text-teal mr-4"/>
+                <span className="text-2xl font-extrabold mb-4 text-brown font-libre-baskerville">123 Main Street, Anytown, USA</span>
+            </div>
+
+            <a href="#" className="text-blue-500 hover:underline">Get Directions ></a>
+        </div>
+
+        <!-- Right Section - Map -->
+        <div className="md:w-2/5 bg-gray-200 rounded-lg p-4 map-container">
+            <!-- Embed your map here -->
+            <div id="map"></div>
+        </div>
+    </div>
+    <ContactUs/>
 </template>
 
 <script>
@@ -8,20 +44,85 @@ import "tailwindcss/tailwind.css";
 import ContactUs from "../components/ContactUs.vue";
 import HeroGlobal from "../components/HeroGlobal.vue";
 
+// Import OpenLayers dependencies
+import "ol/ol.css";
+import Map from "ol/Map";
+import View from "ol/View";
+import {defaults as defaultControls, ScaleLine} from "ol/control";
+import {Tile as TileLayer, Vector as VectorLayer} from "ol/layer";
+import {OSM, Vector as VectorSource} from "ol/source";
+
 export default {
-  name: "ContactPage",
-  components: {
-    ContactUs,
-    HeroGlobal,
-  },
-  data() {
-    return {
-      hero: {
-        title: "CONTACTS",
-      },
-    };
-  },
+    name: "ContactPage",
+    components: {
+        ContactUs,
+        HeroGlobal,
+    },
+    data() {
+        return {
+            hero: {
+                title: "CONTACTS",
+            },
+        };
+    },
+    async mounted() {
+        await this.initiateMap();
+        this.$nextTick(() => {
+            this.map.updateSize();
+        });
+    },
+    methods: {
+        initiateMap() {
+            // create vector layer
+            var source = new VectorSource();
+            var vector = new VectorLayer({
+                source: source,
+            });
+            // create title layer
+            var raster = new TileLayer({
+                source: new OSM(),
+            });
+            // create map with 2 layers
+            this.map = new Map({
+                controls: defaultControls({
+                    attribution: false,  // Disable the attribution control
+                    zoom: true,  // Keep the zoom control
+                }).extend([
+                    new ScaleLine({
+                        units: "degrees",
+                    }),
+                ]),
+                target: "map",
+                layers: [raster, vector],
+                view: new View({
+                    projection: "EPSG:4326",
+                    center: [-79.95, 44.4773],  // Update the center coordinates here
+                    zoom: 8.5,
+                }),
+            });
+        },
+    },
 };
 </script>
+
+<style>
+/* Parent div */
+.map-container {
+    position: relative;
+    height: 50vh;
+}
+
+/* Map container */
+#map {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+}
+
+/* Add other styles as needed */
+</style>
+
 
 <style></style>
