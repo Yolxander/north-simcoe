2a77fe9d984119d0c179530b64d52f8ff8c710f7
Anna <annaspiridonenkova@gmail.com>
2023-10-20 01:12:36 -0400

    remove for merge top nav and footer

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,26 @@
 
 <template>
   <div class="overflow-hidden">
-    <TopNavBar :current-route="$route.path" />
+    <!-- <TopNavBar :current-route="$route.path" /> -->
     <NavBarSide :current-route="$route.path" />
     <router-view />
-    <FooterGlobal />
+    <!-- <FooterGlobal /> -->
   </div>
 </template>
 
 <script>
 import "tailwindcss/tailwind.css";
-import TopNavBar from "./components/NavBarTop.vue";
+// import TopNavBar from "./components/NavBarTop.vue";
 import NavBarSide from "./components/NavBarSide.vue";
-import FooterGlobal from "./components/FooterGlobal.vue";
+// import FooterGlobal from "./components/FooterGlobal.vue";
 
 import router from "./router";
 
 export default {
   components: {
-    TopNavBar,
+    // TopNavBar,
     NavBarSide,
-    FooterGlobal,
+    // FooterGlobal,
   },
   router,
 };
