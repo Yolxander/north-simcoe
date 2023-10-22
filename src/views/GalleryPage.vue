0ea53d479f53ec1fefeb5ed21f1aa224ef098da0
Anna <annaspiridonenkova@gmail.com>
2023-10-21 21:08:22 -0400

    gallery clean up and animation

--- src/views/GalleryPage.vue
+++ src/views/GalleryPage.vue
@@ -1,139 +1,16 @@
 <template>
   <HeroGlobal :hero="hero" />
-  <div class="p-12 md:p-24 text-center">
-    <h3 class="text-3xl font-semibold text-brown font-archivo">
-      See some of the beautiful properties we manage
-    </h3>
-  </div>
-
-  <div
-    class="grid grid-cols-2 md:grid-cols-3 gap-4 pr-8 pl-8 pb-12 md:pl-24 md:pr-12"
-  >
-    <div>
-      <img
-        class="h-auto max-w-full rounded-lg"
-        src="../assets/gallery/gallery1.jpg"
-        alt=""
-      />
+  <ObserverComponent classToToggle="fadeInTop" playOnce="true">
+    <div class="p-12 md:p-24 text-center">
+      <h3 class="text-3xl font-semibold text-brown font-archivo">
+        See some of the beautiful properties we manage
+      </h3>
     </div>
-    <div>
-      <img
-        class="h-auto max-w-full rounded-lg"
-        src="../assets/gallery/gallery2.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <img
-        class="h-auto max-w-full rounded-lg"
-        src="../assets/gallery/gallery6.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <img
-        class="h-auto max-w-full rounded-lg"
-        src="../assets/gallery/gallery4.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <img
-        class="h-auto max-w-full rounded-lg"
-        src="../assets/gallery/gallery5.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <img
-        class="h-auto max-w-full rounded-lg"
-        src="../assets/gallery/gallery3.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <img
-        class="h-auto max-w-full rounded-lg"
-        src="../assets/gallery/gallery13.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <img
-        class="h-auto max-w-full rounded-lg"
-        src="../assets/gallery/gallery8.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <img
-        class="h-auto max-w-full rounded-lg"
-        src="../assets/gallery/gallery9.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <img
-        class="h-auto max-w-full rounded-lg"
-        src="../assets/gallery/gallery14.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <img
-        class="h-auto max-w-full rounded-lg"
-        src="../assets/gallery/gallery16.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <img
-        class="h-auto max-w-full rounded-lg"
-        src="../assets/gallery/gallery12.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <img
-        class="h-auto max-w-full rounded-lg"
-        src="../assets/gallery/gallery7.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <img
-        class="h-auto max-w-full rounded-lg"
-        src="../assets/gallery/gallery10.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <img
-        class="h-auto max-w-full rounded-lg"
-        src="../assets/gallery/gallery11.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <img
-        class="h-auto max-w-full rounded-lg"
-        src="../assets/gallery/gallery17.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <img
-        class="h-auto max-w-full rounded-lg"
-        src="../assets/gallery/gallery15.jpg"
-        alt=""
-      />
-    </div>
-  </div>
-
-  <!-- <div
-    class="grid grid-cols-2 md:grid-cols-4 gap-4 pr-8 pl-8 pb-12 md:pl-24 md:pr-12"
-  >
-    <div class="grid gap-4">
+  </ObserverComponent>
+  <ObserverComponent classToToggle="fadeInBottom" playOnce="true">
+    <div
+      class="grid grid-cols-2 md:grid-cols-3 gap-4 pr-8 pl-8 pb-12 md:pl-24 md:pr-12"
+    >
       <div>
         <img
           class="h-auto max-w-full rounded-lg"
@@ -151,12 +28,10 @@
       <div>
         <img
           class="h-auto max-w-full rounded-lg"
-          src="../assets/gallery/gallery3.jpg"
+          src="../assets/gallery/gallery6.jpg"
           alt=""
         />
       </div>
-    </div>
-    <div class="grid gap-4">
       <div>
         <img
           class="h-auto max-w-full rounded-lg"
@@ -174,30 +49,14 @@
       <div>
         <img
           class="h-auto max-w-full rounded-lg"
-          src="../assets/gallery/gallery6.jpg"
+          src="../assets/gallery/gallery3.jpg"
           alt=""
         />
       </div>
       <div>
         <img
           class="h-auto max-w-full rounded-lg"
-          src="../assets/gallery/gallery14.jpg"
-          alt=""
-        />
-      </div>
-      <div>
-        <img
-          class="h-auto max-w-full rounded-lg"
-          src="../assets/gallery/gallery15.jpg"
-          alt=""
-        />
-      </div>
-    </div>
-    <div class="grid gap-4">
-      <div>
-        <img
-          class="h-auto max-w-full rounded-lg"
-          src="../assets/gallery/gallery7.jpg"
+          src="../assets/gallery/gallery13.jpg"
           alt=""
         />
       </div>
@@ -215,8 +74,34 @@
           alt=""
         />
       </div>
-    </div>
-    <div class="grid gap-4">
+      <div>
+        <img
+          class="h-auto max-w-full rounded-lg"
+          src="../assets/gallery/gallery14.jpg"
+          alt=""
+        />
+      </div>
+      <div>
+        <img
+          class="h-auto max-w-full rounded-lg"
+          src="../assets/gallery/gallery16.jpg"
+          alt=""
+        />
+      </div>
+      <div>
+        <img
+          class="h-auto max-w-full rounded-lg"
+          src="../assets/gallery/gallery12.jpg"
+          alt=""
+        />
+      </div>
+      <div>
+        <img
+          class="h-auto max-w-full rounded-lg"
+          src="../assets/gallery/gallery7.jpg"
+          alt=""
+        />
+      </div>
       <div>
         <img
           class="h-auto max-w-full rounded-lg"
@@ -234,27 +119,19 @@
       <div>
         <img
           class="h-auto max-w-full rounded-lg"
-          src="../assets/gallery/gallery12.jpg"
-          alt=""
-        />
-      </div>
-      <div>
-        <img
-          class="h-auto max-w-full rounded-lg"
-          src="../assets/gallery/gallery13.jpg"
-          alt=""
-        />
-      </div>
-      <div>
-        <img
-          class="h-auto max-w-full rounded-lg"
           src="../assets/gallery/gallery17.jpg"
           alt=""
         />
       </div>
+      <div>
+        <img
+          class="h-auto max-w-full rounded-lg"
+          src="../assets/gallery/gallery15.jpg"
+          alt=""
+        />
+      </div>
     </div>
-  </div> -->
-
+  </ObserverComponent>
   <ContactUs />
 </template>
 
@@ -262,12 +139,14 @@
 import "tailwindcss/tailwind.css";
 import ContactUs from "../components/ContactUs.vue";
 import HeroGlobal from "../components/HeroGlobal.vue";
+import ObserverComponent from "../components/IntersectionObserver.vue";
 
 export default {
   name: "GalleryPage",
   components: {
     ContactUs,
     HeroGlobal,
+    ObserverComponent,
   },
   data() {
     return {
