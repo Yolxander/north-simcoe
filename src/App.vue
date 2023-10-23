2db9beaf82ba35c95e39bd77f48111686b48516d
Anna <annaspiridonenkova@gmail.com>
2023-10-23 00:37:50 -0400

    added nav dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="overflow-hidden">
-    <!-- <TopNavBar :current-route="$route.path" /> -->
+    <TopNavBar :current-route="$route.path" />
     <NavBarSide :current-route="$route.path" />
     <router-view />
     <!-- <FooterGlobal /> -->
@@ -9,7 +9,7 @@
 
 <script>
 import "tailwindcss/tailwind.css";
-// import TopNavBar from "./components/NavBarTop.vue";
+import TopNavBar from "./components/NavBarTop.vue";
 import NavBarSide from "./components/NavBarSide.vue";
 // import FooterGlobal from "./components/FooterGlobal.vue";
 import { computed, reactive } from "vue";
@@ -18,7 +18,7 @@ import router from "./router";
 
 export default {
   components: {
-    // TopNavBar,
+    TopNavBar,
     NavBarSide,
     // FooterGlobal,
   },
