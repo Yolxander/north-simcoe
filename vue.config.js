a09e91721715a1a66d3bace668fd9068c8d81050
yolxander.jaca <yolxander.jaca@humancontact.com>
2025-12-13 17:05:13 -0500

    updated what built HTML shows

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'North Simcoe Property Management in Simcoe County'
+      return args
+    })
+  }
 })
