9e7be530be4e60c1f14023862d62bfd7e7d3616b
yolxander.jaca <yolxander.jaca@humancontact.com>
2026-08-06 20:59:38 -0400

    hide header from embeded section

--- src/views/LandlordContractPage.vue
+++ src/views/LandlordContractPage.vue
@@ -1,10 +1,12 @@
 <template>
-  <section class="fixed top-0 left-0 w-full h-full z-50">
+  <section class="fixed top-0 left-0 w-full h-[100dvh] z-50">
     <iframe
       title="NEW LANDLORD CONTRACT NORTH SIMCOE"
       src="https://eu.jotform.com/sign/262174751376058/invite/01kzcszdpq4a94a0fe4deeba0e?signEmbed=1"
       class="w-full h-full border-0"
+      scrolling="auto"
     ></iframe>
+    <div class="absolute bottom-0 left-0 w-full h-16 bg-white z-10"></div>
   </section>
 </template>
 
@@ -13,13 +15,5 @@ import "tailwindcss/tailwind.css";
 
 export default {
   name: "LandlordContractPage",
-  mounted() {
-    document.documentElement.classList.add("h-full", "overflow-hidden");
-    document.body.classList.add("h-full", "overflow-hidden");
-  },
-  beforeUnmount() {
-    document.documentElement.classList.remove("h-full", "overflow-hidden");
-    document.body.classList.remove("h-full", "overflow-hidden");
-  },
 };
 </script>
