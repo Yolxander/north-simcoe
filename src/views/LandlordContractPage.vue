65b3388b33c72799fde93f23bfc00237b5e70246
yolxander.jaca <yolxander.jaca@humancontact.com>
2026-08-06 21:12:40 -0400

    added form loading splash page

--- src/views/LandlordContractPage.vue
+++ src/views/LandlordContractPage.vue
@@ -3,9 +3,16 @@
     <iframe
       title="NEW LANDLORD CONTRACT NORTH SIMCOE"
       src="https://eu.jotform.com/sign/262174751376058/invite/01kzcszdpq4a94a0fe4deeba0e?signEmbed=1"
-      class="w-full h-full border-0"
+      class="w-full h-full border-0 z-10"
       scrolling="auto"
+      @load="handleLoad"
     ></iframe>
+    <div
+      v-if="isLoading"
+      class="absolute inset-0 bg-teal flex flex-col items-center justify-center z-20"
+    >
+      <h1 class="text-2xl font-archivo font-semibold text-white">New Landlord Contract ...</h1>
+    </div>
   </section>
 </template>
 
@@ -14,5 +21,15 @@ import "tailwindcss/tailwind.css";
 
 export default {
   name: "LandlordContractPage",
+  data() {
+    return {
+      isLoading: true,
+    };
+  },
+  methods: {
+    handleLoad() {
+      this.isLoading = false;
+    },
+  },
 };
 </script>
