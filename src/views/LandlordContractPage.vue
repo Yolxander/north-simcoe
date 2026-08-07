fecb44655e4f34bdec2fa082949059e1423cfe3e
yolxander.jaca <yolxander.jaca@humancontact.com>
2026-08-06 20:41:20 -0400

    added moviel version of the landlord contract

--- src/views/LandlordContractPage.vue
+++ src/views/LandlordContractPage.vue
@@ -1,9 +1,10 @@
 <template>
-  <section class="fixed inset-0 z-50">
+  <section class="fixed top-0 left-0 w-full h-[100dvh] z-50">
     <iframe
       title="NEW LANDLORD CONTRACT NORTH SIMCOE"
       src="https://eu.jotform.com/sign/262174751376058/invite/01kzcszdpq4a94a0fe4deeba0e?signEmbed=1"
       class="w-full h-full border-0"
+      scrolling="auto"
     ></iframe>
   </section>
 </template>
